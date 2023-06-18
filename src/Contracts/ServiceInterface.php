c0561616e88fa0e34918e5fcea67af608f009b47
Cryental <contact@cryental.com>
2023-06-18 18:00:29 +0600

    composer and facade updated

--- src/Contracts/ServiceInterface.php
+++ src/Contracts/ServiceInterface.php
@@ -2,11 +2,14 @@
 
 namespace Volistx\Control\Contracts;
 
+use Volistx\Control\Conns\Status;
+use Volistx\Control\Conns\Subscription;
+
 interface ServiceInterface
 {
     public function boot();
 
-    public function subscription();
+    public function subscription() : Subscription;
 
-    public function status();
+    public function status() : Status;
 }
