b6aaccca1dbeda66c9c003fc0e88fdb87258b82d
Scorias <mostafa.samy203@gmail.com>
2023-07-04 02:47:58 +0300

    bug fixs

--- src/Services/BasicService.php
+++ src/Services/BasicService.php
@@ -3,6 +3,13 @@
 namespace Volistx\Control\Services;
 
 use GuzzleHttp\Client;
+use Volistx\Control\Connections\AdminLog;
+use Volistx\Control\Connections\PersonalToken;
+use Volistx\Control\Connections\Plan;
+use Volistx\Control\Connections\Status;
+use Volistx\Control\Connections\Subscription;
+use Volistx\Control\Connections\User;
+use Volistx\Control\Connections\UserLog;
 
 class BasicService extends AbstractService
 {
@@ -15,5 +22,40 @@ class BasicService extends AbstractService
                 'Accept' => 'application/json',
             ],
         ]);
+    }
+
+    public function status(): Status
+    {
+        return new Status($this->client);
+    }
+
+    public function user(): User
+    {
+        return new User($this->client);
+    }
+
+    public function Subscription($user_id): Subscription
+    {
+        return new Subscription($this->client, $user_id);
+    }
+
+    public function PersonalToken($user_id): PersonalToken
+    {
+        return new PersonalToken($this->client, $user_id);
+    }
+
+    public function AdminLog(): AdminLog
+    {
+        return new AdminLog($this->client);
+    }
+
+    public function UserLog(): UserLog
+    {
+        return new UserLog($this->client);
+    }
+
+    public function Plan(): Plan
+    {
+        return new Plan($this->client);
     }
 }
