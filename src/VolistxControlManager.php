f0397d007f4482e215be0f5d80d5eb32eb7bc905
Cryental <contact@cryental.dev>
2023-10-12 23:27:54 +0600

    For any uses

--- src/VolistxControlManager.php
+++ src/VolistxControlManager.php
@@ -3,16 +3,18 @@
 namespace Volistx\Control;
 
 use Exception;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Config;
 use Volistx\Control\Services\ServiceManager;
 
 class VolistxControlManager
 {
-    public function getService($service): ServiceManager
+    public function getService(string $base_uri, bool $secure, string $access_key): ServiceManager
     {
         // Get service configuration
-        $config = Config::get('volistx-control.services.'.$service, []);
+        $config = [
+            'base_uri' => $base_uri,
+            'secure' => $secure,
+            'access_key' => $access_key
+        ];
 
         // Create service instance
         return new ServiceManager($config);
--- src/VolistxControlProvider.php
+++ src/VolistxControlProvider.php
@@ -15,43 +15,6 @@ class VolistxControlProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->runningInConsole()) {
-            $this->registerResources();
-        }
-
-        if ($this->isLumen() === false) {
-            $this->mergeConfigFrom(__DIR__.'/../config/volistx-control.php', 'volistx-control');
-        }
-
         $this->app->register(VolistxControlServiceProvider::class);
-
-        $this->publishes([
-            __DIR__.'/../config/volistx-control.php' => config_path('volistx-control.php'),
-            __DIR__.'/../database/migrations' => database_path('migrations'),
-        ]);
-    }
-
-    /**
-     * Register resources.
-     *
-     * @return void
-     */
-    public function registerResources()
-    {
-        if ($this->isLumen() === false) {
-            $this->publishes([
-                __DIR__.'/../config/volistx-control.php' => config_path('volistx-control.php'),
-            ], 'config');
-        }
-    }
-
-    /**
-     * Check if package is running under Lumen app
-     *
-     * @return bool
-     */
-    protected function isLumen()
-    {
-        return Str::contains($this->app->version(), 'Lumen') === true;
     }
 }
